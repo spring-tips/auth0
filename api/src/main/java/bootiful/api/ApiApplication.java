7ead6f10c904b4f99dcadd8dc73c8ac5f54ecaa9
Josh Long <josh@joshlong.com>
2024-08-11 04:19:13 -0700

    signing

--- api/src/main/java/bootiful/api/ApiApplication.java
+++ api/src/main/java/bootiful/api/ApiApplication.java
@@ -1,16 +1,11 @@
 package bootiful.api;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
-import org.springframework.security.oauth2.core.OAuth2Error;
-import org.springframework.security.oauth2.core.OAuth2TokenValidator;
-import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult;
-import org.springframework.security.oauth2.jwt.*;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.security.Principal;
 import java.util.Map;
 
+// https://developer.auth0.com/resources/labs/authorization/spring-boot-microservices-security#overview
 @SpringBootApplication
 public class ApiApplication {
 
@@ -34,42 +30,9 @@ public class ApiApplication {
                 .oauth2ResourceServer(ors -> ors.jwt(c -> c.decoder(jwtDecoder)))
                 .build();
     }
-
-    @Bean
-    JwtDecoder jwtDecoder(
-            @Value("${auth0.audience}") String audience,
-            @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}") String issuer
-    ) {
-        NimbusJwtDecoder jwtDecoder = JwtDecoders.fromOidcIssuerLocation(issuer);
-        OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(audience);
-        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
-        OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
-
-        jwtDecoder.setJwtValidator(withAudience);
-
-        return jwtDecoder;
-    }
 }
 
-class AudienceValidator implements OAuth2TokenValidator<Jwt> {
 
-    private final String audience;
-
-    AudienceValidator(String audience) {
-        this.audience = audience;
-    }
-
-    @Override
-    public OAuth2TokenValidatorResult validate(Jwt jwt) {
-        OAuth2Error error = new OAuth2Error("invalid_token", "The required audience is missing", null);
-
-        if (jwt.getAudience().contains(audience)) {
-            return OAuth2TokenValidatorResult.success();
-        }
-
-        return OAuth2TokenValidatorResult.failure(error);
-    }
-}
 
 @Controller
 @ResponseBody
